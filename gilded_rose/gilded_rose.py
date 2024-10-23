fb25c188df2954557e7eb28ddcf9fe74bf7ae4c4
Fabien Huitelec <fabien@huitelec.fr>
2024-10-23 13:25:56 +0000

    refactor: move age logic to item

--- gilded_rose/gilded_rose.py
+++ gilded_rose/gilded_rose.py
@@ -5,6 +5,13 @@ def update_quality(items):
     new_items = []
     for old_item in items:
         item = copy.deepcopy(old_item)
+
+        if item.name != "Aged Brie"\
+            and item.name != "Backstage passes to a TAFKAL80ETC concert"\
+            and item.name != "Sulfuras, Hand of Ragnaros":
+            new_items.append(item.age())
+            continue
+
         if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
             if item.quality > 0:
                 if item.name != "Sulfuras, Hand of Ragnaros":
@@ -32,6 +39,7 @@ def update_quality(items):
             else:
                 if item.quality < 50:
                     item.quality = item.quality + 1
+
         new_items.append(item)
     
     return new_items
@@ -40,8 +48,33 @@ def update_quality(items):
 class Item:
     def __init__(self, name, ennobles, legendary, backstage, sell_in, quality):
         self.name = name
+        self.ennobles = ennobles
+        self.legendary = legendary
+        self.backstage = backstage
         self.sell_in = sell_in
         self.quality = quality
+
+    def age(self) -> "Item":
+        quality_multiplier = 2 if self.sell_in <= 0 else 1
+
+        sell_in = self.sell_in - 1
+        quality = max(self.quality - quality_multiplier, 0)
+
+        if self.ennobles:
+            quality = min(self.quality + quality_multiplier, 50)
+
+        if self.legendary:
+            sell_in = self.sell_in
+            quality = self.quality
+
+        return Item(
+            self.name,
+            self.ennobles,
+            self.legendary,
+            self.backstage,
+            sell_in,
+            quality
+        )
 
     def __repr__(self):
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
