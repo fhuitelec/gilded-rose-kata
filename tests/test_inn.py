418dd175a50d9ef5c56a2756fdbc4726a9df4a45
Fabien Huitelec <fabien.huitelec.partner@decathlon.com>
2025-08-05 08:40:50 +0200

    tests: add test intention with a builder

--- tests/test_inn.py
+++ tests/test_inn.py
@@ -2,17 +2,16 @@
 
 import pytest
 
-from gilded_rose.inn import GildedRose, Item
-
-AGED_BRIE = "Aged Brie"
+from gilded_rose.inn import GildedRose
+from tests.builder import an_item
 
 
 def test_sell_in_decreases_over_time():
     """Test items' sell-in date decreases over time."""
     # Arrange
     items = [
-        Item("foo", sell_in=5, quality=3),
-        Item("bar", sell_in=4, quality=3),
+        an_item().sell_in(5).with_quality(3).build(),
+        an_item().sell_in(4).with_quality(3).build(),
     ]
     gilded_rose = GildedRose(items)
 
@@ -29,8 +28,8 @@ def test_quality_degrades_over_time():
     """Test items' quality degrades over time."""
     # Arrange
     items = [
-        Item("foo", sell_in=5, quality=3),
-        Item("bar", sell_in=4, quality=3),
+        an_item().sell_in(5).with_quality(3).build(),
+        an_item().sell_in(4).with_quality(3).build(),
     ]
     gilded_rose = GildedRose(items)
 
@@ -46,9 +45,7 @@ def test_quality_degrades_over_time():
 def test_quality_cannot_get_degrades_twice_as_fast_when_item_is_expired():
     """Test items' quality degrades twice as fast when an item's sell-in date has passed."""
     # Arrange
-    items = [
-        Item("foo", sell_in=0, quality=3),
-    ]
+    items = [an_item().expired().with_quality(3).build()]
     gilded_rose = GildedRose(items)
 
     # Act
@@ -62,9 +59,7 @@ def test_quality_cannot_get_degrades_twice_as_fast_when_item_is_expired():
 def test_quality_cannot_get_negative():
     """Test items' quality cannot become negative."""
     # Arrange
-    items = [
-        Item("foo", sell_in=2, quality=0),
-    ]
+    items = [an_item().expired().with_quality(0).build()]
     gilded_rose = GildedRose(items)
 
     # Act
@@ -77,9 +72,7 @@ def test_quality_cannot_get_negative():
 def test_aged_brie_ennobles_with_time():
     """Test aged bries' quality increases with time."""
     # Arrange
-    items = [
-        Item(AGED_BRIE, sell_in=2, quality=2),
-    ]
+    items = [an_item().ennobling().with_quality(2).build()]
     gilded_rose = GildedRose(items)
 
     # Act
@@ -92,9 +85,7 @@ def test_aged_brie_ennobles_with_time():
 def test_aged_brie_ennobles_twice_as_fast_when_it_is_expired():
     """Test aged bries' quality increases twice as fast once the items' sell-in date has passed."""
     # Arrange
-    items = [
-        Item(AGED_BRIE, sell_in=0, quality=2),
-    ]
+    items = [an_item().ennobling().expired().with_quality(2).build()]
     gilded_rose = GildedRose(items)
 
     # Act
@@ -107,9 +98,7 @@ def test_aged_brie_ennobles_twice_as_fast_when_it_is_expired():
 def test_quality_cannot_exceed_50():
     """Test items' quality cannot go over 50."""
     # Arrange
-    items = [
-        Item(AGED_BRIE, sell_in=1, quality=50),
-    ]
+    items = [an_item().ennobling().with_quality(50).build()]
     gilded_rose = GildedRose(items)
 
     # Act
@@ -123,5 +112,8 @@ def test_quality_cannot_exceed_50():
 def test_quality_cannot_exceed_50_when_instanciated():
     """Test items' quality cannot go over 50 at its creation."""
     # Arrange
+    builder = an_item().ennobling().with_quality(51)
+
+    # Act & assert
     with pytest.raises(ValueError):
-        Item(AGED_BRIE, sell_in=1, quality=50)
+        builder.build()
