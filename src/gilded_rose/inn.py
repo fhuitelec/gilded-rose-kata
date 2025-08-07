8a3a88de74b3e30cd39b2519dede4646a1708ace
Fabien Huitelec <fabien.huitelec.partner@decathlon.com>
2025-08-07 14:09:04 +0000

    refactor: move ennobling behaviour in Item

--- src/gilded_rose/inn.py
+++ src/gilded_rose/inn.py
@@ -19,23 +19,29 @@ class Item:  # pylint: disable=too-few-public-methods
     sell_in: int
     quality: int
 
+    @property
     def _ennobling(self):
         """Returns whether the item ennobles over time."""
         return self.name == AGED_BRIE
 
+    @property
     def _legendary(self):
         """Returns whether the item is legendary or not."""
         return self.name == SULFURAS
 
+    @property
     def _backstage_passes(self):
         """Returns whether the item is a backstage pass or not."""
         return self.name == BACKSTAGE_PASSES
 
     def compute_after_a_day(self) -> "Item":
         """Compute the state of the item after a day."""
-        quality_decrease = 1 if self.sell_in > 0 else 2
+        quality_variation = 1 if self.sell_in > 0 else 2
 
-        quality = max(0, self.quality - quality_decrease)
+        quality = max(0, self.quality - quality_variation)
+
+        if self._ennobling:
+            quality = min(50, self.quality + quality_variation)
 
         return Item(self.name, sell_in=self.sell_in - 1, quality=quality)
 
@@ -49,14 +55,11 @@ def update_quality(items: list[Item]):  # pylint: disable=too-many-branches
     for old_item in items:  # pylint: disable=too-many-nested-blocks
         item = copy.deepcopy(old_item)
 
-        if item.name not in (AGED_BRIE, BACKSTAGE_PASSES, SULFURAS):
+        if item.name not in (BACKSTAGE_PASSES, SULFURAS):
             new_items.append(item.compute_after_a_day())
             continue
 
-        if (
-            item.name != "Aged Brie"  # pylint: disable=consider-using-in
-            and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        ):
+        if item.name != "Backstage passes to a TAFKAL80ETC concert":
             if item.quality > 0:
                 if item.name != "Sulfuras, Hand of Ragnaros":
                     item.quality = item.quality - 1
@@ -73,16 +76,12 @@ def update_quality(items: list[Item]):  # pylint: disable=too-many-branches
         if item.name != "Sulfuras, Hand of Ragnaros":
             item.sell_in = item.sell_in - 1
         if item.sell_in < 0:
-            if item.name != "Aged Brie":
-                if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                    if item.quality > 0:
-                        if item.name != "Sulfuras, Hand of Ragnaros":
-                            item.quality = item.quality - 1
-                else:
-                    item.quality = item.quality - item.quality
+            if item.name != "Backstage passes to a TAFKAL80ETC concert":
+                if item.quality > 0:
+                    if item.name != "Sulfuras, Hand of Ragnaros":
+                        item.quality = item.quality - 1
             else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
+                item.quality = item.quality - item.quality
         new_items.append(item)
 
     return new_items
